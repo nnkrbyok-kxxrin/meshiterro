c6d65ca871ff74e49e7a707a93abe4894f5455d3
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-01-16 14:45:57 +0000

    一時保存

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '87ed24063f9008d086107b07d9400a8c19f368268db2820d0551880b00ef1263dd30bc9ccaab640a21f9afa07868bf897bba254e06fe5ea9c6cdb5ebe246524a'
